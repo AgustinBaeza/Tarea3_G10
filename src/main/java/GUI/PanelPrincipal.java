4fdd80c6ef22b493e934483c305feddf554b0351
Alan Flores <floresyerey@gmail.com>
2026-06-03 20:13:47 -0400

    Integre PanelPrincipal con comprador y expendedor. Actualice PanelPrincipal para dejar de usar un depósito de prueba y pasar a funcionar como contenedor principal de la interfaz gráfica. Ahora el panel crea y conecta el Expendedor lógico, el Comprador lógico, el PanelExpendedor y el PanelComprador y así poder verlo en la misma ventana. También incluí MouseAdapter este hace que al hacer cada click obtiene sus coordenadas y se envía a los paneles para que luego se llame a repaint() para actualizar la interfaz gráfica.

--- src/main/java/GUI/PanelPrincipal.java
+++ src/main/java/GUI/PanelPrincipal.java
@@ -4,20 +4,46 @@ import javax.swing.*;
 import java.awt.*;
 import Logica.*;
 import Logica.producto.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelPrincipal extends JPanel {
 
-    private PanelDeposito<Bebida> panelDeposito;
+    private Expendedor expendedor;
+    private Comprador comprador;
+    private PanelExpendedor panelExpendedor;
+    private PanelComprador panelComprador;
 
     public PanelPrincipal() {
-        Deposito<Bebida> dep = new Deposito<>();
-        panelDeposito = new PanelDeposito<>(50,50,300,100,dep);
+        expendedor = new Expendedor(10);
+        comprador = new Comprador(expendedor);
+        panelExpendedor = new PanelExpendedor(20, 20, 760, 580, expendedor);
+        panelComprador = new PanelComprador(820, 20, 330, 580, comprador);
+
+        setBackground(Color.WHITE);
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                int mouseX = e.getX();
+                int mouseY = e.getY();
+
+                System.out.println("Click en X: " + mouseX + " Y: " + mouseY);
+
+                panelExpendedor.verificarClick(mouseX, mouseY);
+                panelComprador.procesarClick(mouseX, mouseY);
+
+                repaint();
+            }
+        });
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        panelDeposito.paintComponent(g);
+        panelExpendedor.paintComponent(g);
+        panelComprador.paintComponent(g);
     }
 }
